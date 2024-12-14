2ebc94de925eb4c700a6aad690e26845b7f0074e
maksimkaran <turtleln7000@gmail.com>
2024-12-15 00:04:28 +0100

    helath_bar_change

--- Assets/Scripts/health_bar.cs
+++ Assets/Scripts/health_bar.cs
@@ -4,7 +4,7 @@ public class health_bar : MonoBehaviour
 {
 
     public Slider slider;
-
+    public Gradient gradient;
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
