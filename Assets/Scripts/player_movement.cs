e6d0d67c13f0fb8d6b60f7c1d3c01edb0315e3a5
maksimkaran <turtleln7000@gmail.com>
2024-12-15 02:19:40 +0100

    added space station and started planet factory sistem

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -7,7 +7,8 @@ public class player_movement : MonoBehaviour
     // Speed of the spaceship
     public float moveSpeed = 5f;
     public float rotationSpeed = 200f; // Speed of rotation (for turning)
-
+    private bool isAnimating = false;
+    public Animator animator;
     // Rigidbody2D component for physics-based movement
     private Rigidbody2D rb;
 
@@ -15,10 +16,20 @@ public class player_movement : MonoBehaviour
     {
         // Get the Rigidbody2D component attached to the spaceship
         rb = GetComponent<Rigidbody2D>();
+
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W))  // 0 is for the left mouse button
+        {
+            isAnimating = true;  // Set animation to true
+        }
+        if (Input.GetKeyUp(KeyCode.W))  // 0 is for the left mouse button
+        {
+            isAnimating = false;  // Set animation to true
+        }
+        animator.SetBool("gas", isAnimating);
         // Get input for movement
         float moveHorizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right Arrow for rotation
         float moveVertical = Input.GetAxis("Vertical");     // W/S or Up/Down Arrow for forward/backward movement
